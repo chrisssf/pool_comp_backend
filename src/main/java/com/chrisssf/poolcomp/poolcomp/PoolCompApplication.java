ee414ba815d9001b571e42dbcfd5c4e2c9d90d8e
Chris Fraser <chrisssfraser@gmail.com>
2020-09-23 21:40:34 +0100

    project created using Spring Initializr

---
+++ src/main/java/com/chrisssf/poolcomp/poolcomp/PoolCompApplication.java
@@ -0,0 +1,13 @@
+package com.chrisssf.poolcomp.poolcomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolCompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrisssf/poolcomp/poolcomp/PoolCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisssf.poolcomp.poolcomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
